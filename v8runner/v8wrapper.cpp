0ea62454de569cbf7237e869c234894aa380b3d6
iFlex <milorad.liviu.felix@gmail.com>
2015-10-11 21:28:30 +0100

    added support for js load() function in v8wrapper

--- v8runner/v8wrapper.cpp
+++ v8runner/v8wrapper.cpp
@@ -8,11 +8,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "include/libplatform/libplatform.h"
 #include "include/v8.h"
 
 using namespace v8;
+
+void ReportException(v8::Isolate* isolate, v8::TryCatch* handler);
+bool ExecuteString(v8::Isolate* isolate,
+                   v8::Handle<v8::String> source,
+                   v8::Handle<v8::Value> name,
+                   bool print_result,
+                   bool report_exceptions);
+void Load(const v8::FunctionCallbackInfo<v8::Value>& args);
 
 class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
  public:
@@ -31,11 +40,11 @@ char * readSourceScript(char * file){
         if(!lstat(file,&fileInfo)){
 		printf("Source size %lld\n",fileInfo.st_size);
 		source = new char[fileInfo.st_size+1];
-		char chrBuff;		
+		char chrBuff;
 		FILE * fileH = fopen(file,"rb");
-		if(fileH){	
+		if(fileH){
 			while(fread(&chrBuff,sizeof(char),1,fileH)) source[index++]=chrBuff;
-			fclose(fileH);		
+			fclose(fileH);
 		}
 	}
 	return source;
@@ -45,7 +54,7 @@ int main(int argc, char* argv[]) {
   char * sourceScript = 0;
   if(argc > 1)
 	sourceScript = readSourceScript(argv[1]);
-  
+
   if(!sourceScript)
 	return 0;
 
@@ -67,8 +76,12 @@ int main(int argc, char* argv[]) {
     // Create a stack-allocated handle scope.
     HandleScope handle_scope(isolate);
 
+    v8::Handle<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
+    global->Set(v8::String::NewFromUtf8(isolate, "load"),
+                v8::FunctionTemplate::New(isolate, Load));
+
     // Create a new context.
-    Local<Context> context = Context::New(isolate);
+    Local<Context> context = v8::Context::New(isolate, NULL, global);
 
     // Enter the context for compiling and running the hello world script.
     Context::Scope context_scope(context);
@@ -97,4 +110,135 @@ int main(int argc, char* argv[]) {
   delete sourceScript;
   printf("\nDone\n");
   return 0;
+}
+
+// Extracts a C string from a V8 Utf8Value.
+const char* ToCString(const v8::String::Utf8Value& value) {
+  return *value ? *value : "<string conversion failed>";
+}
+
+// Reads a file into a v8 string.
+v8::Handle<v8::String> ReadFile(v8::Isolate* isolate, const char* name) {
+  FILE* file = fopen(name, "rb");
+  if (file == NULL) return v8::Handle<v8::String>();
+
+  fseek(file, 0, SEEK_END);
+  int size = ftell(file);
+  rewind(file);
+
+  char* chars = new char[size + 1];
+  chars[size] = '\0';
+  for (int i = 0; i < size;) {
+    int read = static_cast<int>(fread(&chars[i], 1, size - i, file));
+    i += read;
+  }
+  fclose(file);
+  v8::Handle<v8::String> result =
+      v8::String::NewFromUtf8(isolate, chars, v8::String::kNormalString, size);
+  delete[] chars;
+  return result;
+}
+
+// The callback that is invoked by v8 whenever the JavaScript 'load'
+// function is called.  Loads, compiles and executes its argument
+// JavaScript file.
+void Load(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  for (int i = 0; i < args.Length(); i++) {
+    v8::HandleScope handle_scope(args.GetIsolate());
+    v8::String::Utf8Value file(args[i]);
+    if (*file == NULL) {
+      args.GetIsolate()->ThrowException(
+          v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file"));
+      return;
+    }
+    v8::Handle<v8::String> source = ReadFile(args.GetIsolate(), *file);
+    if (source.IsEmpty()) {
+      args.GetIsolate()->ThrowException(
+           v8::String::NewFromUtf8(args.GetIsolate(), "Error loading file"));
+      return;
+    }
+    if (!ExecuteString(args.GetIsolate(),
+                       source,
+                       v8::String::NewFromUtf8(args.GetIsolate(), *file),
+                       false,
+                       false)) {
+      args.GetIsolate()->ThrowException(
+          v8::String::NewFromUtf8(args.GetIsolate(), "Error executing file"));
+      return;
+    }
+  }
+}
+
+// Executes a string within the current v8 context.
+bool ExecuteString(v8::Isolate* isolate,
+                   v8::Handle<v8::String> source,
+                   v8::Handle<v8::Value> name,
+                   bool print_result,
+                   bool report_exceptions) {
+  v8::HandleScope handle_scope(isolate);
+  v8::TryCatch try_catch;
+  v8::ScriptOrigin origin(name);
+  v8::Handle<v8::Script> script = v8::Script::Compile(source, &origin);
+  if (script.IsEmpty()) {
+    // Print errors that happened during compilation.
+    if (report_exceptions)
+      ReportException(isolate, &try_catch);
+    return false;
+  } else {
+    v8::Handle<v8::Value> result = script->Run();
+    if (result.IsEmpty()) {
+      assert(try_catch.HasCaught());
+      // Print errors that happened during execution.
+      if (report_exceptions)
+        ReportException(isolate, &try_catch);
+      return false;
+    } else {
+      assert(!try_catch.HasCaught());
+      if (print_result && !result->IsUndefined()) {
+        // If all went well and the result wasn't undefined then print
+        // the returned value.
+        v8::String::Utf8Value str(result);
+        const char* cstr = ToCString(str);
+        printf("%s\n", cstr);
+      }
+      return true;
+    }
+  }
+}
+
+void ReportException(v8::Isolate* isolate, v8::TryCatch* try_catch) {
+  v8::HandleScope handle_scope(isolate);
+  v8::String::Utf8Value exception(try_catch->Exception());
+  const char* exception_string = ToCString(exception);
+  v8::Handle<v8::Message> message = try_catch->Message();
+  if (message.IsEmpty()) {
+    // V8 didn't provide any extra information about this error; just
+    // print the exception.
+    fprintf(stderr, "%s\n", exception_string);
+  } else {
+    // Print (filename):(line number): (message).
+    v8::String::Utf8Value filename(message->GetScriptOrigin().ResourceName());
+    const char* filename_string = ToCString(filename);
+    int linenum = message->GetLineNumber();
+    fprintf(stderr, "%s:%i: %s\n", filename_string, linenum, exception_string);
+    // Print line of source code.
+    v8::String::Utf8Value sourceline(message->GetSourceLine());
+    const char* sourceline_string = ToCString(sourceline);
+    fprintf(stderr, "%s\n", sourceline_string);
+    // Print wavy underline (GetUnderline is deprecated).
+    int start = message->GetStartColumn();
+    for (int i = 0; i < start; i++) {
+      fprintf(stderr, " ");
+    }
+    int end = message->GetEndColumn();
+    for (int i = start; i < end; i++) {
+      fprintf(stderr, "^");
+    }
+    fprintf(stderr, "\n");
+    v8::String::Utf8Value stack_trace(try_catch->StackTrace());
+    if (stack_trace.length() > 0) {
+      const char* stack_trace_string = ToCString(stack_trace);
+      fprintf(stderr, "%s\n", stack_trace_string);
+    }
+  }
 }
