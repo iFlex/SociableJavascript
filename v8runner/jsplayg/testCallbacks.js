88d1709665308822c0e55a29490c7060d3fc8217
iFlex <milorad.liviu.felix@gmail.com>
2015-11-12 13:32:27 +0000

    added todos

--- v8runner/jsplayg/testCallbacks.js
+++ v8runner/jsplayg/testCallbacks.js
@@ -13,23 +13,14 @@ try {
  //heapSize = 50;
  size = 1024*20;
  var items = [];
+
  for( var i = 0; i < 1000; ++i ){
  	print((i+1)*size+"B"+" av:"+_getHeapAvailable()+" hs:"+_getHeapSize());
  	
-
- 	if( _getHeapAvailable() < 5) {
- 		
- 		//if(heapSize < 1000)
- 		//	heapSize *= 2;
-
- 		//print("Setting max old space to:"+heapSize+"MB");
- 		//_setMaxHeapSize(800+"");
- 		//print("Getting heap size:"+_getHeapSize());
- 	}
-
  	if( i == 625 )
  		_setMaxHeapSize("800");
- 	//generate 1 MB
+ 	
+	//generate 1 MB
  	items.push(makeid(size));
  }
 
