694efab4cdd5922e2f9c2e2165de8fe106ca8ab7
Milorad Felix <2021082f@sibu.(none)>
2016-02-22 22:34:44 +0000

    tweaking install script

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
 installdr=$1
 echo "INSTALL_DIR:"$installdr
 cd $installdr
-echo "Installing required software tools..."
+echo "-- Installing required software tools..."
 echo "Installing git"
 sudo apt-get install git
 echo "Installing wget"
@@ -14,35 +14,33 @@ echo "installing depot tools"
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=pwd/depot_tools:"$PATH"
 
-echo "Cloning necessary repositories..."
+echo "-- Cloning necessary repositories..."
 echo "cloning JsonCpp"
 git clone https://github.com/open-source-parsers/jsoncpp.git
 echo "cloning SociableJavascript"
 git clone https://github.com/iFlex/SociableJavascript.git
 
-echo "Building dependencies"
+echo "-- Building dependencies"
 echo "> Building JsonCpp"
 cd jsoncpp
 mkdir -p build/release
 cd build/release
 cmake -DCMAKE_BUILD_TYPE=release -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DARCHIVE_INSTALL_DIR=. -G "Unix Makefiles" ../..
 make
-echo "*> JsonCpp Built"
 echo "Moving JsonCpp library in the right place for the V8 build process... ($installdr/custom_libs/)"
 mkdir $installdr/custom_libs
 cp ./src/lib_json/libjsoncpp.a $installdr/custom_libs/
-echo "Moved successfully"
-cd ../../..
 
-echo "Downloading Modified V8"
-wget  -O $installdr/ "http://linktov8zipfile"
+cd $installdr
+echo "-- Downloading Modified V8"
+wget  -O $installdr/v8.zip "http://linktov8zipfile"
 unzip v8.zip
 cd v8
-echo "Building V8 ..."
+echo "-- Building V8 ..."
 make native -j8
 
+cd $installdr
 echo "Building V8Wrapper..."
-cd ../SociableJavascript/v8runner/
-./build
-
-echo "READY"
+cd SociableJavascript/v8runner/
+./build.bash
+echo "-- DONE --"
