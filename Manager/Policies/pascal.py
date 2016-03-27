e1134b0c5d03e15a9859e6ea55a914aab4ab967d
iFlex <milorad.liviu.felix@gmail.com>
2016-03-27 04:15:47 +0100

    Policies cleaned

--- Manager/Policies/pascal.py
+++ Manager/Policies/pascal.py
@@ -1,47 +1,18 @@
-#Pascal Policy - puts pressure on isolates to use less memory
+#Pascal2 Policy - uses soft heap limit to put pressure on isolates
 #Author@ Milorad Liviu Felix
 import math
 
 def init(context):
 	pass
 
-def markIsolates(isolates,totalAvailableMemory):
-	memlim = totalAvailableMemory / len(isolates)
-	hasNewIsolates = False;
-	
-	for isolate in isolates:
-		if "pMark" not in isolate:
-			isolate["pMark"] = True;
-			isolate["average"] = 0;
-			isolate["avindex"] = 0;
-			hasNewIsolates = True
-
-	if hasNewIsolates:
-		for isolate in isolates:
-			isolate["hardHeapLimit"] = memlim;
-			
-	return hasNewIsolates
-
 def calculate(totalAvailableMemory,isolates,ctx):
-	if markIsolates(isolates,totalAvailableMemory):
-		return isolates;
-
 	for i in isolates:
-		if i["throughput"] < 0.1:
-			i["hardHeapLimit"] *= 2;
-			i["average"] = i["hardHeapLimit"]
-			i["avindex"] = 1
-		else:
-			i["average"] += i["footPrint"];
-			i["avindex"] += 1
-			mul = 1
-			i["hardHeapLimit"] = int(i["average"]/(i["avindex"]*mul))
-			#i["hardHeapLimit"] /= 2;
-			
+		i["softHeapLimit"] = 0;
+	
 	return isolates
 
 def name():
-	return "Pascal v1.0"
+	return "Pascal v2.0"
 
 def stats():
 	return "No stats available"
